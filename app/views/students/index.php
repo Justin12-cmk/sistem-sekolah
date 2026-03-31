8b812e56eccbfde61aa76cf27eeec75c369fafb3
Justin12-cmk <jusyijhshsi@gmail.com>
2026-03-31 07:54:34 +0700

    Feat: implement student index page

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar Siswa</title>
     <link rel="stylesheet" href="/css/output.css">
-</head>
+</head>         
 <!-- Header -->
 <body class="min-h-screen flex flex-col bg-gray-100">
     <header class="bg-blue-500 text-white">
@@ -17,16 +17,44 @@
     </header>
     <!--Main-->
     <main class="bg-gray-100 grow container mx-auto ">
-        <div class="at-8">
-
+        <div class="at-8 space-y-4">
+            <!--Card Header-->
+            <div class="bg-white shadow rounded-lg p-4">
+                <h1 class="text-xl font-bold">Daftar Siswa</h1>
+                <p>Menampilkan daftar Siswa yang terdaftar</p>
+            </div>
+            <div class="bg-white shadow rounded-lg p-4">
+                <table class="w-full">
+                    <thead class="bg-gray-200">
+                        <tr>
+                            <th class="py-2 px-4 text-left">Nomor</th>
+                            <th class="py-2 px-4 text-left">Nama</th>
+                            <th class="py-2 px-4 text-left">Nis</th>
+                            <th class="py-2 px-4 text-left">Kelas</th>
+                            <th class="py-2 px-4 text-left">Nomor telepon</th>
+                            <th class="py-2 px-4 ">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="py-2 px-4 text-left">1</td>
+                            <td class="py-2 px-4 text-left">Pokemon</td>
+                            <td class="py-2 px-4 text-left">1234</td>
+                            <td class="py-2 px-4 text-left">XITKJ3</td>
+                            <td class="py-2 px-4 text-left">0812345678</td>
+                            <td class="py-2 px-4 text-left">
+                                <div class="flex items-center justify-center gap-4">
+                                            <a href="" class="text-green-500">Detail</a>
+                                            <a href="" class="text-yellow-500">Edit</a>
+                                            <a href="" class="text-red-500">Hapus</a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <div>
-                  <!--Card Header-->
-                  <div class="bg-white shadow rounded-lg p-4">
-                    <h1 class="text-xl font-bold">Daftar Siswa</h1>
-                    <p>Menampilkan daftar Siswa yang terdaftar</p>
-                  </div>
-        </div>
+    </main>
 <!--Card Header End-->
 <!--Card Content-->
 <div class="bg-white shadow rounded-lg">
