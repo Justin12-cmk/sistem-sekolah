3b9402db3076df5161ca0c5a95ffeb2b5578ab1e
Justin12-cmk <jusyijhshsi@gmail.com>
2026-04-17 11:34:50 +0700

    feat:implement student detail page

--- app/views/students/create.php
+++ app/views/students/create.php
@@ -1,37 +1,39 @@
-        <div class="mt-8 space-y-4">
-            <!--Card Header start-->
-            <div class="bg-white shadow rounded-lg p-4">
-                <h1 class="text-2xl font-bold ">Detail Siswa</h1>
-                <p>Menampilkan detail siswa yang terdaftar</p>
+<div class="mt-8 space-y-4">
+    <!-- Card Header Start -->
+    <div class="bg-white shadow rounded-lg p-4">
+        <h1 class="text-2xl font-bold">Tambah Siswa</h1>
+        <p>Menambahkan siswa ke dalam sistem</p>
+    </div>
+    <!-- Card Header End -->
+
+    <!-- Card Content Start -->
+    <div class="bg-white shadow rounded-lg">
+        <form action="" class="p-4 grid grid-cols-2 gap-4">
+            <div class="space-y-2">
+                <label class="block font-bold" for="name">Nama</label>
+                <input class="w-full border rounded-lg py-2 px-4" type="text" name="name" id="name"
+                    placeholder="Masukkan nama">
             </div>
-            <!--Card Header End-->
-            <!--Card Content start-->
-            <div class="bg-white shadow rounded-lg p-4">
-                <div class="p-4 grid grid-cols-2 gap-4">
-                    <div class="space-y-2">
-                        <label for="name" class="block font-bold">Nama</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="name" id="name"
-                            placeholder="Masukkan nama" readonly>
-                    </div>
-                    <div class="space-y-2">
-                        <label for="name" class="block font-bold">NIS</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="nis" id="nis"
-                            placeholder="Masukkan NIS" readonly>
-                    </div>
-                    <div class="space-y-2">
-                        <label for="name" class="block font-bold">Kelas</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="kelas" id="kelas"
-                            placeholder="Masukkan Kelas" readonly>
-                    </div>
-                    <div class="space-y-2">
-                        <label for="name" class="block font-bold">No Telepon</label>
-                        <input class="w-full border rounded-lg py-2 px-4" type="text" name="phone_number" id="phone_number"
-                            placeholder="Masukkan No Telepon" readonly>
-                    </div>
-                    <div class="flex justify-end gap-4 col-span-2">
-                        <a href="/students" class="py-2 px-4 bg-gray-200 rounded-lg">Kembali</a>                    </div>
-                </div>
+            <div class="space-y-2">
+                <label class="block font-bold" for="nis">NIS</label>
+                <input class="w-full border rounded-lg py-2 px-4" type="text" name="nis" id="nis"
+                    placeholder="Masukkan NIS">
             </div>
-            <!--Card Content End-->
-        </div>
- 
+            <div class="space-y-2">
+                <label class="block font-bold" for="class">Kelas</label>
+                <input class="w-full border rounded-lg py-2 px-4" type="text" name="class" id="class"
+                    placeholder="Masukkan Kelas">
+            </div>
+            <div class="space-y-2">
+                <label class="block font-bold" for="phone_number">No Telepon</label>
+                <input class="w-full border rounded-lg py-2 px-4" type="text" name="phone_number" id="phone_number"
+                    placeholder="Masukkan No Telepon">
+            </div>
+            <div class="flex justify-end gap-4 col-span-2">
+                <a href="/students" class="py-2 px-4 bg-gray-100 rounded-lg">Kembali</a>
+                <button type="submit" class="py-2 px-4 bg-blue-500 rounded-lg text-white">Simpan</button>
+            </div>
+        </form>
+    </div>
+    <!-- Card Content End -->
+</div>
